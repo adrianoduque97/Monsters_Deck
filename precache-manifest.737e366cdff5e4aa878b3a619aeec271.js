17a97717429dd79ff46cf740e09009541cfefe5b
adrianoduque97 <adrian.duque@capmation.com>
2020-07-02 22:13:12 -0500

    Updates

---
+++ precache-manifest.737e366cdff5e4aa878b3a619aeec271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4470550a75ee80f1fc38f96ee2b6cce",
+    "url": "/Monsters_Deck/index.html"
+  },
+  {
+    "revision": "c916e278403369748ba6",
+    "url": "/Monsters_Deck/static/css/main.fbfa55ad.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters_Deck/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters_Deck/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c916e278403369748ba6",
+    "url": "/Monsters_Deck/static/js/main.99189af8.chunk.js"
+  },
+  {
+    "revision": "8d176f83b46fa7c088b0",
+    "url": "/Monsters_Deck/static/js/runtime-main.8920f174.js"
+  }
+]);
